4b24e34baacb033ec36e148370635014aa0387e9
Chris Wheatley <chris.wheatley@sky.uk>
2017-09-28 08:48:22 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+var html = require('choo/html')
+var choo = require('choo')
+
+var app = choo()
+app.use(titleStore)
+app.route('/', mainView)
+app.mount('body')
+
+function mainView (state, emit) {
+  return html`
+    <div>
+      <h1>Title: ${state.title}</h1>
+      <input type="text" value="${state.title}" oninput=${update} />
+    </div>
+  `
+
+  function update (e) {
+    emit('update', e.target.value)
+  }
+}
+
+function titleStore (state, emitter) {
+  state.title = 'Not quite set yet'
+  emitter.on('DOMContentLoaded', function () {
+    emitter.on('update', function (newTitle) {
+      state.title = newTitle
+      emitter.emit('render')
+    })
+  })
+}
